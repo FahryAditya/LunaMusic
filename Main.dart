f2b7ee985be5d5ad506e6f2a62b3b20c79f23397
Fahry Aditya Setiawan <Fahryadityagithub@gmail.com>
2025-11-16 20:23:00 +0800

    Create main.dart for LunaMusic application
    Add main entry point for the LunaMusic app.

---
+++ Main.dart
@@ -0,0 +1,10 @@
+// lib/main.dart
+
+import 'package:flutter/material.dart';
+import 'luna_music.dart';
+
+void main() {
+  // Pastikan Anda memanggil API_BASE_URL yang benar untuk backend Anda.
+  // Misalnya: http://192.168.1.100:3000
+  runApp(const LunaMusicApp());
+}
